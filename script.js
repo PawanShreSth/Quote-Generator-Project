28af1e9d3bbe3af95ab07884e4b472472382cdbe
Pawan Shrestha <pawanshresthait@gmail.com>
2021-07-27 12:53:55 +0545

    Added loading animation

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
+// SHow Loading
+
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
 
 // Get Quote From API
 
@@ -44,8 +51,7 @@ function tweetQuote() {
 
 
 // Event Listeners
-
 newQuoteBtn.addEventListener('click', getQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 
-//getQuote()
+//loading();
